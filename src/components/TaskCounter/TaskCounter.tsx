f886c4dd9ba54254b52b8d0f74a62e7b2e354e04
Ira Prysiazhna <ira.prisyazhna@gmail.com>
2025-04-01 10:38:19 +0300

    task counter by using progress bar

--- src/components/TaskCounter/TaskCounter.tsx
+++ src/components/TaskCounter/TaskCounter.tsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
+import s from "./TaskCounter.module.css";
 
 export default function TaskCounter() {
   // Отримуємо масив завдань із стану Redux
@@ -11,22 +12,31 @@ export default function TaskCounter() {
   };
 
   // На базі стану Redux отримуємо похідні дані
-  const count = tasks.reduce(
-    (accumulator: TaskCount, task: { completed: boolean }) => {
-      if (task.completed) {
-        accumulator.completed += 1;
-      } else {
-        accumulator.active += 1;
-      }
-      return accumulator;
-    },
-    { active: 0, completed: 0 } as TaskCount
-  );
+  // const count = tasks.reduce(
+  //   (accumulator: TaskCount, task: { completed: boolean }) => {
+  //     if (task.completed) {
+  //       accumulator.completed += 1;
+  //     } else {
+  //       accumulator.active += 1;
+  //     }
+  //     return accumulator;
+  //   },
+  //   { active: 0, completed: 0 } as TaskCount
+  // );
+
+  const completedTasks = tasks.filter((task) => task.completed).length;
+  const totalTasks = tasks.length;
+  const progress = totalTasks ? (completedTasks / totalTasks) * 100 : 0;
 
   return (
     <div>
-      <p>Active: {count.active}</p>
-      <p>Completed: {count.completed}</p>
+      {/* <p>Active: {count.active}</p>
+      <p>Completed: {count.completed}</p> */}
+      <h3>Progress:</h3>
+      <progress className={s.progress} value={progress} max="100"></progress>
+      <p>
+        {completedTasks} / {totalTasks} tasks completed
+      </p>
     </div>
   );
 }
